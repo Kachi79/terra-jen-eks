da2cfd67e32e0a20a662e625ba9d5eace7a4141e
Harris <kacharris747@gmail.com>
2024-03-31 20:24:13 +0100

    version upgraded

--- eks/eks.tf
+++ eks/eks.tf
@@ -1,9 +1,8 @@
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
-  # version = "~> 19.0"
 
   cluster_name    = "kaira-eks-cluster"
-  cluster_version = "1.24"
+  cluster_version = "1.28"
 
   cluster_endpoint_public_access = true
 
