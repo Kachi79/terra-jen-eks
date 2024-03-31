e85eb949e0d8043f39cbca3ce86c516d96f0ae19
Harris <kacharris747@gmail.com>
2024-03-31 15:25:42 +0100

    more modifications

--- eks/vpc.tf
+++ eks/vpc.tf
@@ -1,4 +1,3 @@
-# VPC
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -23,32 +22,5 @@ module "vpc" {
   private_subnet_tags = {
     "kubernetes.io/cluster/koko-eks-role" = "shared"
     "kubernetes.io/role/internal-elb"   = 1
-  }
-}
-
-module "eks" {
-  source = "terraform-aws-modules/eks/aws"
-
-  cluster_name    = "koko-eks-cluster"
-  cluster_version = "1.28"
-
-  cluster_endpoint_public_access = true
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  eks_managed_node_groups = {
-    node = {
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
-
-      instance_type = ["t2.small"]
-    }
-  }
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
   }
 }
