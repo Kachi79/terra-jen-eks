0da8f96265681d8fb7bce9e1ddd45889dc7d3206
Harris <kacharris747@gmail.com>
2024-03-31 18:19:21 +0100

    iam role

--- eks/eks.tf
+++ eks/eks.tf
@@ -19,6 +19,28 @@ module "eks" {
     }
   }
 
+   # Cluster access entry
+  # To add the current caller identity as an administrator
+  enable_cluster_creator_admin_permissions = true
+
+  access_entries = {
+    # One access entry with a policy associated
+    example = {
+      kubernetes_groups = []
+      principal_arn     = "arn:aws:iam::471112779887:user/kachi" 
+
+      policy_associations = {
+        example = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
+          access_scope = {
+            namespaces = ["default"]
+            type       = "namespace"
+          }
+        }
+      }
+    }
+  }
+
   tags = {
     Environment = "dev"
     Terraform   = "true"
